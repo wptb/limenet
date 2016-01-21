011473bfe46b8135827f9fae854b298d330d7277
root <root@baghira.gamca.sk>
2016-01-21 22:14:22 +0100

    ok

--- docker/recipes/run.rb
+++ docker/recipes/run.rb
@@ -1,6 +1,5 @@
-bash "run_docker" do
+bash "docker-pusti" do
   user "root"
-  cwd "/etc/openvpn"
   code <<-EOH
     docker run -v /mnt/openvpn:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN kylemanna/openvpn
   EOH
