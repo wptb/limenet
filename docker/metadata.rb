3738ccfb8b83ee23fec0235728e934ad488a762d
root <root@baghira.gamca.sk>
2016-01-22 15:21:34 +0100

    metadata zmena

--- docker/metadata.rb
+++ docker/metadata.rb
@@ -1,7 +1,7 @@
-name             'docker'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+name             'docker+vpn'
+maintainer       'wptb'
+maintainer_email 'wptb@limenet.sk'
 license          'All rights reserved'
-description      'Installs/Configures docker'
+description      'Installs/Configures docker and OpenVPN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
