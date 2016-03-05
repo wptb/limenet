d4e36ff1e92c15bb3b969ca2b63ee2d272b94641
wptb <stevo.chromik@gmail.com>
2016-03-05 13:28:54 +0100

    Spliting to new repo

--- docker/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'docker+vpn'
-maintainer       'wptb'
-maintainer_email 'wptb@limenet.sk'
-license          'All rights reserved'
-description      'Installs/Configures docker and OpenVPN'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
